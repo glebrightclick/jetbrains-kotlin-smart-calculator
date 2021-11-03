af8130c302f0df611eb9ac09cd677fd501a9b08a
Gleb Vinogradov <g.vinogradov@corp.badoo.com>
2021-11-03 16:19:30 +0300

    [master]: task N5 Description Modify your program to handle different cases when the given expression has an invalid format. The program should print Invalid expression in such cases. The program must never throw the NumberFormatException or any other exception.
    If a user enters an invalid command, the program must print Unknown command.
    
    All messages must be printed without quotes.
    
    Do not forget to write methods to decompose your program.

--- Main.kt
+++ Main.kt
@@ -2,41 +2,76 @@ package calculator
 
 fun main() {
     while (true) {
-        when (val input = readLine()!!.toString()) {
-            "/exit" -> {
-                println("Bye!")
-                break
-            }
-            "/help" -> {
-                println(
-                    "The program calculates mathematical expressions" +
-                    "Only add/subtract operations are supported"
-                )
-            }
-            "" -> continue
-            else -> {
-                var string = input.replace(Regex(" +"), " ")
-                do {
-                    val previousString = string
-                    string = string
-                        .replace(Regex("\\++"), "+")
-                        .replace(Regex("(\\+-)|(-\\+)"), "-")
-                        .replace("--", "+")
-                } while (previousString != string)
-
-                var result = 0
-                var action : Operation = Operation.ADD
-                val expression = string.split(" ")
-                for (variable in expression) {
-                    when (variable) {
-                        Operation.ADD.string -> action = Operation.ADD
-                        Operation.SUBTRACT.string -> action = Operation.SUBTRACT
-                        else -> result = action.calculate(result, variable.toInt())
-                    }
+        try {
+            when (val input = readLine()!!.toString()) {
+                "/exit" -> {
+                    println("Bye!")
+                    break
                 }
+                "/help" -> {
+                    println(
+                        "The program calculates mathematical expressions" +
+                        "Only add/subtract operations are supported"
+                    )
+                }
+                "" -> continue
+                else -> {
+                    require(isValidInput(input))
 
-                println(result)
+                    val string = format(input)
+                    var result = 0
+                    var operation: Operation? = Operation.ADD
+                    val expression = string.split(" ")
+                    for (variable in expression) {
+                        when (variable) {
+                            Operation.ADD.string -> operation = Operation.ADD
+                            Operation.SUBTRACT.string -> operation = Operation.SUBTRACT
+                            else -> {
+                                require(checkInputVariable(result, operation, variable))
+
+                                result = operation!!.calculate(result, variable.toInt())
+                                operation = null
+                            }
+                        }
+                    }
+
+                    require(checkResult(result, operation))
+                    println(result)
+                }
             }
+        } catch (unknownCommandException : UnknownCommandException) {
+            println("Unknown command")
+        } catch (exception : Exception) {
+            println("Invalid expression")
         }
     }
 }
+
+class UnknownCommandException() : Exception()
+
+fun format(input : String) : String {
+    var string = input.replace(Regex(" +"), " ")
+    do {
+        val previousString = string
+        string = string
+            .replace(Regex("\\++"), "+")
+            .replace(Regex("(\\+-)|(-\\+)"), "-")
+            .replace("--", "+")
+            .replace(Regex("\\+([0-9]+)"), fun(matches: MatchResult) = matches.groupValues[1])
+    } while (previousString != string)
+
+    return string
+}
+
+fun isValidInput(input : String) : Boolean {
+    if (input.startsWith('/')) throw UnknownCommandException()
+
+    return true
+}
+
+fun checkInputVariable(result : Int, storedOperation : Operation?, variable : String) =
+    storedOperation !== null
+        && variable == variable.toInt().toString()
+
+fun checkResult(result : Int, storedOperation : Operation?) : Boolean =
+    storedOperation === null
